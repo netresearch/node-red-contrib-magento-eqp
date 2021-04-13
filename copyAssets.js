8fcc6ceb3f51301483df1a33f329672ce490e9fd
TheDevMinerTV <tobigames200@gmail.com>
2021-04-13 09:41:19 +0200

    Add custom icons for the nodes

--- copyAssets.js
+++ copyAssets.js
@@ -5,6 +5,8 @@ const tsConfig = require('./tsconfig.json');
 
 const SRC_FOLDER = Path.join(__dirname, 'src');
 const DIST_FOLDER = Path.join(__dirname, 'dist');
+const ICON_FOLDER = Path.join(__dirname, 'icons');
+const DIST_ICON_FOLDER = Path.join(__dirname, 'dist', 'icons');
 
 if (FS.existsSync(DIST_FOLDER)) {
 	console.log('Deleting dist folder...');
@@ -13,11 +15,14 @@ if (FS.existsSync(DIST_FOLDER)) {
 }
 
 FS.mkdirSync(DIST_FOLDER);
+FS.mkdirSync(DIST_ICON_FOLDER);
 
 const sourceFiles = FS.readdirSync(SRC_FOLDER);
+const iconsFiles = FS.readdirSync(ICON_FOLDER);
 
 const tsFiles = sourceFiles.filter((f) => f.endsWith('.ts'));
 const htmlFiles = sourceFiles.filter((f) => f.endsWith('.html'));
+const iconFiles = iconsFiles.filter((f) => f.endsWith('.png') || f.endsWith('.svg'));
 
 tsFiles.forEach((f) => {
 	const splitFileName = f.split('.');
@@ -39,4 +44,10 @@ htmlFiles.forEach((f) => {
 	console.log(`[Frontend] "${f}"...`);
 
 	FS.copyFileSync(Path.join(SRC_FOLDER, f), Path.join(DIST_FOLDER, f));
+});
+
+iconFiles.forEach((f) => {
+	console.log(`[Icons] Copying "${f}"...`);
+
+	FS.copyFileSync(Path.join(ICON_FOLDER, f), Path.join(DIST_ICON_FOLDER, f));
 });
